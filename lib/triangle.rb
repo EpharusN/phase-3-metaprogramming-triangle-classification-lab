b0c803da4f3d7b25484e9c8e03dd7c659314cf05
Epharus Ndumia <epharusndumia@gmail.com>
2023-05-28 04:25:45 +0300

    initial commit

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+    validate_triangle
+  end
+
+  class TriangleError < StandardError
+  end
+
+  def kind
+    if side1 == side2 && side2 == side3
+      :equilateral
+    elsif side1 == side2 || side2 == side3 || side3 == side1
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  private
+
+  def validate_triangle
+    validate_positive_sides
+    validate_triangle_inequality
+  end
+
+  def validate_positive_sides
+    raise TriangleError, "Side lengths must be greater than 0" if side1 <= 0 || side2 <= 0 || side3 <= 0
+  end
+
+  def validate_triangle_inequality
+    unless side1 + side2 > side3 && side2 + side3 > side1 && side3 + side1 > side2
+      raise TriangleError, "Invalid triangle: Triangle inequality violation"
+    end
+  end
 end
